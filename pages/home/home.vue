f259eec8fe7a43ad906c5a589434c8b4ce917e8f
monikaismonika <126456731+monikaismonika@users.noreply.github.com>
2023-07-01 11:00:07 +0800

    完成轮播图以及相关二级分包的转跳 (#8)
    完成轮播图以及相关二级分包的转跳

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,26 +1,43 @@
 <template>
 	<view>
-		home
+		<swiper class="home_swiper" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000"
+			:circular="true">
+			<swiper-item v-for="(item,index) in swiperMsg" :key="item">
+				<navigator class="swiper-item" :url="`/subpkg/good_detail/goods_detail?goods_id=${item.goods_id}`">
+					<image :src="item.image_src" mode="aspectFit" style="height: 340rpx;"></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
 <script>
+	import {
+		mapActions
+	} from 'vuex'
 	export default {
 		data() {
-			return {};
+			return {
+				swiperMsg: {},
+			};
 		},
 		methods: {
-			test() {
-				return uni._request({
+			_getSwiperMsg() {
+				return this.$store.dispatch('home/_getSwiper', {
 					method: 'GET',
-					url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems',
-				});
+					url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+				})
 			}
 		},
-		mounted() {},
+		async mounted() {
+			this.swiperMsg = await this._getSwiperMsg();
+			console.log(this.swiperMsg);
+		},
 	}
 </script>
 
 <style lang="scss">
-
+	// .home_swiper {
+	// 	margin-top: 0px;
+	// }
 </style>
--- store/home/index.js
+++ store/home/index.js
@@ -1,9 +1,26 @@
-const state = {};
-const mutations = {};
-const actions = {};
-const getters = {
-
+const state = {
+	swiperMsg: {}
 };
+const getters = {
+	_swiperMsgGetter(state) {
+		return state.swiperMsg;
+	}
+};
+const mutations = {
+	GETSWIPER(state, result) {
+		this.swiperMsg = result;
+	}
+};
+const actions = {
+	async _getSwiper({
+		commit
+	}, reqMsg) {
+		const result = await uni._request(reqMsg);
+		result == 'error' ? {} : commit('GETSWIPER', result.message);
+		return result.message;
+	}
+};
+
 export default {
 	namespaced: true,
 	state,
